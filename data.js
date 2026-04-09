9c1fbcd957c3fb36de943be2ffbc0e1a1bde7118
vanush5512-svg <vanush5512@gmail.com>
2026-04-09 20:50:02 +0300

    Update data from admin panel

--- data.js
+++ data.js
@@ -4,7 +4,7 @@ var ARM={
   _g:function(k,d){if(!this._admin)return d;try{var v=localStorage.getItem('arm_'+k);return v?JSON.parse(v):d}catch(e){return d}},
   _s:function(k,v){if(!this._admin)return;try{localStorage.setItem('arm_'+k,JSON.stringify(v))}catch(e){}},
   cats:function(v){return v!==undefined?(this._s('cats',v),v):this._g('cats',[{"id":1,"icon":"🥩","name":"Мясные блюда","desc":"Хоровац, кебаб, бастурма"},{"id":2,"icon":"🍲","name":"Супы","desc":"Хаш, спас, бозбаш"},{"id":3,"icon":"🫓","name":"Выпечка","desc":"Лаваш, матнакаш, гата"},{"id":4,"icon":"🍯","name":"Десерты","desc":"Пахлава, суджух"},{"id":5,"icon":"🥤","name":"Напитки","desc":"Кофе, тан, компоты"}])},
-  prods:function(v){return v!==undefined?(this._s('prods',v),v):this._g('prods',[{"name":"333","catId":1,"price":333,"weight":"","emoji":"🍽","badge":"","desc":"","img":"","id":1775756821933}])},
+  prods:function(v){return v!==undefined?(this._s('prods',v),v):this._g('prods',[{"name":"123123","catId":1,"price":123123,"weight":"","emoji":"🍽","badge":"","desc":"123123","img":"","id":1775757000688}])},
   orders:function(v){return v!==undefined?(this._s('orders',v),v):this._g('orders',[])},
   del:function(v){return v!==undefined?(this._s('del',v),v):this._g('del',{"min":300,"price":100,"free":1500,"time":"30–60 минут"})},
   settings:function(v){return v!==undefined?(this._s('settings',v),v):this._g('settings',{"phone":"+7 (999) 123-45-67","hours":"Ежедневно 10:00–22:00","pwd":"123123","city":"Аркадак"})}
